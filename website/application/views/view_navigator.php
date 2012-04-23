1544ca50f5af19443ba5e321c9d22ef7fc42ccf1
Peter Donin <peter@donin.net>
2012-04-23 08:15:03 +0300

    Missing navigator files

---
+++ website/application/views/view_navigator.php
@@ -0,0 +1,5 @@
+<div class="menus">
+            <div class="li"><a class="clear_link" href="<?php echo site_url('can_help'); ?>">Я МОЖУ ДОПОМОГТИ</a> <div class="black_line<?php echo $page=='can_help'?' activ':''?>"></div></div>
+            <div class="li"><a class="clear_link" href="<?php echo site_url('need_help'); ?>">ПОТРIБНА ДОПОМОГА</a> <div class="black_line<?php echo $page=='need_help'?' activ':''?>"></div></div>
+            <div class="li small"><span><a class="clear_link" href="<?php echo site_url('about_us'); ?>">ПРО НАС</a></span><div class="black_line<?php echo $page=='about_us'?' activ':''?>"></div></div>
+</div>
